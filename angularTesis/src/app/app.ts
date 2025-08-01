ee929d3468bafe52df17dafb9a287bb267a9c70b
saraagutierrez <149322564+saraagutierrez@users.noreply.github.com>
2025-08-01 13:36:47 -0500

    Corrección barra de menú

--- angularTesis/src/app/app.ts
+++ angularTesis/src/app/app.ts
@@ -41,33 +41,3 @@ export class App {
 }
 
 
-/*import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterModule, NgIf, NgFor, NgClass, AsyncPipe, RouterOutlet],
-  templateUrl: './app.html',
-  styleUrls: ['./app.css']
-})
-export class App {
-  protected title = 'angularTesis';
-
-  hideMenu = false;
-
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this.hideMenu = event.urlAfterRedirects === '/main';
-      });
-  }
-
-  logout(): void {
-    localStorage.clear();
-    this.router.navigate(['/main']);
-  }
-}*/
